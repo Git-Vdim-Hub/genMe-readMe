8f3941279b0b156fb9ec75183d14d7bc3ae7b510
Crypto_Vdim <vadimrace@gmail.com>
2023-01-21 18:09:50 -0500

    created license badge, link, section, connected markdown generation function, finished description section

--- index.js
+++ index.js
@@ -1,15 +1,70 @@
-// TODO: Include packages needed for this application
+//Packages needed for this application
 const inquirer = require('inquirer');
-const markDown = require('./utils/generateMarkdown');
+const markDown = require('./utils/generateMarkdown.js');
 const fs = require('fs');
-// TODO: Create an array of questions for user input
-const questions = [];
+//Array of questions for user input
+const questions = ['What is the name of your project? ', 'Please describe the project: ', 'What are the installation instructions? ',
+                   'What is the usage information? ', 'What license are you using?', 'Please provide contribution guidelines: ', 'What are the test instructions? '];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    const file = `${fileName.toLowerCase().split(' ').join(' ')}.md`;
+    const writtenStuff = markDown.generateMarkdown(data);
+    fs.writeFile(file, writtenStuff, (err) =>
+    err ? console.log(err) : console.log('Title')
+    )
+    // fs.appendFile(file, descTitle, (err) =>
+    // err ? console.log(err) : console.log('Description Title')
+    // )
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: questions[0]
+            },
+            {
+                type: 'input',
+                name: 'desc',
+                message: questions[1]
+            },
+            // {
+            //     type: 'input',
+            //     name: 'install',
+            //     message: questions[2]
+            // },
+            // {
+            //     type: 'input',
+            //     name: 'usage',
+            //     message: questions[3]
+            // },
+             {
+                 type: 'list',
+                 name: 'license',
+                 message: questions[4],
+                 choices: ['MIT', 'Apache', 'Boost', 'BSD']
+             },
+            // {
+            //     type: 'input',
+            //     name: 'contribution',
+            //     message: questions[5]
+            // },
+            // {
+            //     type: 'input',
+            //     name: 'test',
+            //     message: questions[6]
+            // },
+        ])
+        .then((data) => {
+            //console.log(data.name);
+            console.log(data.license);
+            writeToFile(data.name, data);
+        })
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,38 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+   if(license === 'MIT'){
+    return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+   }  else{
+    return '';
+   }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    if(license === 'MIT'){
+        return '[MIT](https://opensource.org/licenses/MIT)';
+       }  else{
+        return '';
+       }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+    return `This project is licensed under the ${renderLicenseLink(license)} license\n`
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  return `# ${data.name}\n
+  ${renderLicenseBadge(data.license)}\n
+  ## Description\n
+  ${data.desc}\n
+  ## License\n
+  ${renderLicenseSection(data.license)}\n
+  `.replaceAll('  ', '');
 }
 
-module.exports = generateMarkdown;
+module.exports = {generateMarkdown};
